1aa618f24de99f0f42b56ef5b12b4062dd0a196b
JeevakumaranS <jeevas0028@gmail.com>
2026-08-25 13:54:33 +0530

    optimize model

--- app.py
+++ app.py
@@ -21,15 +21,23 @@ model.load_state_dict(
 )
 
 model.eval()
-
 def prediction(image_path):
+
     image = Image.open(image_path)
+
     image = image.resize((224, 224))
+
     input_data = TF.to_tensor(image)
+
     input_data = input_data.view((-1, 3, 224, 224))
-    output = model(input_data)
+
+    with torch.no_grad():
+        output = model(input_data)
+
     output = output.detach().numpy()
+
     index = np.argmax(output)
+
     return index
 
 
